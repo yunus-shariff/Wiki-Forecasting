53aa0836b00f5eafbe903126270071131b671868
y-unu5 <shariff.yunus96@gmail.com>
2022-11-28 21:39:12 -0500

    Update wiki_traffic.py

--- wiki_traffic.py
+++ wiki_traffic.py
@@ -27,7 +27,6 @@ from tqdm import tqdm
 # from statsmodels.graphics.tsaplots import plot_acf
 import datetime 
 import calendar
-import hiplot as hip
 
 header  = st.container()
 dataset = st.container()
@@ -351,8 +350,7 @@ with trends:
     plt.ylabel('Total traffic(10^8)')
     plt.legend(labels =['Chinese','Spanish','French','Russian','English','NA','German','Japanese'])
     st.pyplot(fig)
-    
-    hip.Experiment.from_csv('https://media.githubusercontent.com/media/yunus-shariff/Wiki-Forecasting/main/train_1.csv')
+
     
     st.markdown("The Media audience contributes the most in terms of traffic even though the number of media articles is tied for fourth place")
     st.markdown("Although there are a lot more inferences to be drawn in terms of languages and a combination of access type, let's answer some burning questions!")
